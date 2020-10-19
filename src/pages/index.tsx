69a138c2e2d654c4da1d89d0e8e9f168654dde13
Arden de Raaij <a.de.raaij@gmail.com>
2020-10-19 23:01:07 +0100

    add loadingMore spinner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -79,8 +79,12 @@ const Index: React.FC = () => {
             <button
               onClick={() => loadMorePosts()}
               disabled={loadingMorePosts}
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="flex mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
+              {' '}
+              {loadingMorePosts && (
+                <div className="loader mr-2 ease-linear rounded-full border-2 border-t-2 border-gray-200 h-6 w-6"></div>
+              )}
               {loadingMorePosts ? 'Loading...' : 'Show More'}
             </button>
           </div>
