ba468a240867913352280aeb6f1179349c76d818
Arden de Raaij <a.de.raaij@gmail.com>
2020-10-28 22:02:14 +0000

    remove animatepresence

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from '../lib/apolloClient';
-import { AnimatePresence } from 'framer-motion';
 import '../styles/index.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps, router }) => {
@@ -13,9 +12,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps, router }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <ApolloProvider client={apolloClient}>
-        <AnimatePresence>
-          <Component {...pageProps} key={router.route} />
-        </AnimatePresence>
+        <Component {...pageProps} key={router.route} />
       </ApolloProvider>
     </>
   );
