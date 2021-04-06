040a75e1a6c11b1cff433113e87e3413a782a512
Arden de Raaij <a.de.raaij@gmail.com>
2021-04-06 21:27:43 +0100

    add header styles

--- src/components/Date/index.tsx
+++ src/components/Date/index.tsx
@@ -1,10 +1,14 @@
 import { parseISO, format } from 'date-fns';
-
+import styles from './Date.module.css';
 interface Props {
   dateString: string;
 }
 const Date: React.FC<Props> = ({ dateString }) => {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>;
+  return (
+    <time className={styles['time']} dateTime={dateString}>
+      {format(date, 'LLLL	d, yyyy')}
+    </time>
+  );
 };
 export default Date;
