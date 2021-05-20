2593a6fc46f90e5bf2af12f9eb51177c205d01ba
Arden de Raaij <a.de.raaij@gmail.com>
2021-05-20 12:40:36 +0100

    add footer start

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,33 @@
+import styles from './Footer.module.scss';
+
+const Footer: React.FC<any> = ({}) => {
+  return (
+    <footer className={styles['footer']}>
+      <div className={styles['inner']}>
+        <ul className={styles['link-list']}>
+          <li>
+            <a>About</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+          <li>
+            <a>Copyright</a>
+          </li>
+          <li>
+            <a>Terms & Conditions</a>
+          </li>
+          <li>
+            <a>Privacy Policy</a>
+          </li>
+        </ul>
+        <span className={styles['tagline']}>
+          Slangin' Dope <br /> Since 2008
+        </span>
+        <img className={styles['logo']} src="/images/CFYE_NEW.svg" />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
